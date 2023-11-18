be373e1d67f45fa94ad64366f1b8b6b026b0a59a
deepakjaat07 <deepakjhajhari4657@gmail.com>
2023-11-18 14:35:04 +0530

    sec1,2,3,4

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,33 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'Gilroy': ['Gilroy'],
+        'Konexy':['Konexy Personal Use'],
+        'Inter':['Inter']
+      },
+    },
+    boxShadow: {
+      'card': '0px 20px 60.57143px 0px #E5E9F6',
+    },
+    colors: {
+      'white':'#ffffff',
+      'secondaryWhite':'#F6F6F6',
+      'black':'#000000',
+      'secondaryBlack':'#0D0D0D',
+      'purple':'#6022EA',
+      'pink':'#FA00FF',
+    },
+    container:{
+      padding:'14px',
+    },
+    screens:{
+      'sm':'640px',
+      'md':'768px',
+      'lg':'992px',
+      'xl':'1320px',
+    },
   },
   plugins: [],
 }
